61e61555de7eba7809df0eb19567b47cfc09ffae
程安絮 <74345648+ChengAnXu2014@users.noreply.github.com>
2021-04-17 18:17:52 +0800

    Update KuaiJi.py
    Use list instead of dict as suptitles stack, make the source file smeller.

--- KuaiJi.py
+++ KuaiJi.py
@@ -1,9 +1,8 @@
-import os
 import re
 import sublime
 import sublime_plugin
 
-class KuaijiFindCommand(sublime_plugin.TextCommand):
+class Test5Command(sublime_plugin.TextCommand):
 	def run(self, edit):
 		def on_done(index):
 			if index >= 0:
@@ -12,31 +11,25 @@ class KuaijiFindCommand(sublime_plugin.TextCommand):
 
 		titles_list=[]
 		supTitles_list=[]
-		supTitles_dict={}
-		supTitles=u''
-		titleRgx=u'^\.+[^.].*(?=:$)'
-		preRgx=u'.'
+		supSeparate='/'
+		preRgx='.'
+		titleRgx='^'+re.escape(preRgx)+'+[^'+preRgx+'].*(?=:$)'
 
 
 		titleRegions_list=self.view.find_all(titleRgx)
 		for titleRegion in titleRegions_list:
 			title=self.view.substr(titleRegion)
-			(hierPre,title)=re.match(u'(^\.+)([^.].*)', title).groups()
-			supTitles_dict[hierPre]=title
-			preIndex=preRgx
-			while hierPre != preIndex:
-				if preIndex not in supTitles_dict:
-					supTitles_dict[preIndex]=u'缺失'
+			(hierPre,title)=re.match('(^'+re.escape(preRgx)+'+)([^'+preRgx+'].*)', title).groups()
+			while len(supTitles_list) < len(hierPre):
+				if len(supTitles_list) == len(hierPre)-1:
+					supTitles_list.append(title)
+				else:
+					supTitles_list.append('miss')
+
+			supTitles_list[len(hierPre)-1]=title
+
+			titles_list.append([title,supSeparate.join(supTitles_list[0:len(hierPre)-1])])
 
 
-				supTitles_list.append(supTitles_dict[preIndex]+u'/')
-				preIndex+=preRgx
-
-
-			titles_list.append([title,supTitles.join(supTitles_list)])
-			supTitles_list=[]
-			supTitles=u''
-
-
-		sublime.status_message(u'找到'+str(len(titles_list))+u'个标题')
+		sublime.status_message('found '+str(len(titles_list))+' titles')
 		self.view.window().show_quick_panel(titles_list, on_done)
